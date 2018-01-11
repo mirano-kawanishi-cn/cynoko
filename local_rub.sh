6e013d7b0ee1fcb3dbbd46099ebf8231e3ed3b72
河西未良之 <mirano-kawanishi@cnsr.jp>
2018-01-11 18:07:44 +0900

    first commit

---
+++ local_rub.sh
@@ -0,0 +1,4 @@
+export HUBOT_CHATWORK_TOKEN="61c74ae06c3eda90770e4cdd0f65f1a3" # APIのアクセストークンを入力します。
+export HUBOT_CHATWORK_ROOMS="57414369"   # カンマ区切りでルームIDを指定します。
+export HUBOT_CHATWORK_API_RATE="350"   # 1時間あたりのAPIリクエスト数(間隔)を指定します。
+bin/hubot -a chatwork -n cynoko
